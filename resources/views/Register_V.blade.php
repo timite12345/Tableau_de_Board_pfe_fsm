655794245801051fec42107caa68c9efa6524888
timite12345 <timite1145@gmail.com>
2023-03-02 02:45:27 +0100

    first commit

--- resources/views/Register_V.blade.php
+++ resources/views/Register_V.blade.php
@@ -38,38 +38,38 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
 <nav class="navbar navbar-transparent navbar-absolute">
-<div class="container">
-<div class="navbar-header">
-<button type="button" class="navbar-toggle navbar-toggle-black" data-toggle="collapse" data-target="#navigation-example-2">
-<span class="sr-only">Toggle navigation</span>
-<span class="icon-bar "></span>
-<span class="icon-bar "></span>
-<span class="icon-bar"></span>
-</button>
-</div>
-<div class="collapse navbar-collapse">
-<ul class="nav navbar-nav navbar-right">
-<li>
-<a href='{{url("Login_V")}}' class="btn">
-Looking to login?
-</a>
-</li>
-</ul>
-</div>
-</div>
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle navbar-toggle-black" data-toggle="collapse" data-target="#navigation-example-2">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar "></span>
+                <span class="icon-bar "></span>
+                <span class="icon-bar"></span>
+            </button>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href='{{url("Login_V")}}' class="btn">
+                        Looking to login?
+                        </a>
+                    </li>
+                </ul>
+            </div>
+    </div>
 </nav>
 <div class="wrapper wrapper-full-page">
 <div class="register-page">
 
 <div class="content">
-<div class="container">
-<div class="row">
-<div class="col-md-8 col-md-offset-2">
-<div class="header-text">
-<h2>Ivoire Secours Dashboard </h2>
-<h4>Enregistrer un adminsitrateur.</h4>
-<hr>
-</div>
+    <div class="container">
+        <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+        <div class="header-text">
+        <h2>Ivoire Secours Dashboard </h2>
+        <h4>Enregistrer un adminsitrateur.</h4>
+        <hr>
+    </div>
 </div>
 <div class="col-md-4 col-md-offset-2">
  <div class="media">
